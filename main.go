87b45e658aad4108128a5d14c29f551799855e4c
Alexey Baranov <a.baranov@office.ngs.ru>
2016-08-05 15:19:43 +0700

    add options to upload package to repo

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ Description required.
 
 Usage:
     repoctl [options] [-v] -L
-    repoctl [options] [-v] (-L|-A|-S|-E|-R) <repo> <epoch> <db> <arch>
+    repoctl [options] [-v] -L <repo> <epoch> <db> <arch>
+    repoctl [options] [-v] (-A|-S|-E|-R) <repo> <epoch> <db> <arch> [<package>] [--file=<path>]
     repoctl -h | --help
     repoctl --version
 
@@ -33,6 +34,8 @@ Options:
       <epoch>                 Specify repository epoch.
       <db>                    Specify repository db.
       <arch>                  Specify repository architecture.
+      <package>               Specify package to manipulate with.
+      --file=<path>           Package file to add or edit in repository.
     --json                    Print output in json format.
     -h --help                 Show this help.
 `
@@ -53,10 +56,12 @@ func main() {
 		modeEdit   = args["--edit"].(bool)
 		modeRemove = args["--remove"].(bool)
 
-		repo, _  = args["<repo>"].(string)
-		epoch, _ = args["<epoch>"].(string)
-		db, _    = args["<db>"].(string)
-		arch, _  = args["<arch>"].(string)
+		repo, _        = args["<repo>"].(string)
+		epoch, _       = args["<epoch>"].(string)
+		db, _          = args["<db>"].(string)
+		arch, _        = args["<arch>"].(string)
+		packageName, _ = args["<package>"].(string)
+		packageFile, _ = args["--file"].(string)
 
 		jsonOutput = args["--json"].(bool)
 	)
@@ -80,12 +85,33 @@ func main() {
 		}
 	}
 
-	client.appendURLParts(repo, epoch, db, arch)
+	if packageFile != "" {
+		currentDirectory, err := os.Getwd()
+		if err != nil {
+			reportError(err)
+		}
+
+		err = client.LoadPackageFile(currentDirectory + "/" + packageFile)
+		if err != nil {
+			reportError(err)
+		}
+
+		if packageName == "" {
+			packageName = packageFile
+		}
+	}
+
+	client.appendURLParts([]string{
+		repo,
+		epoch,
+		db,
+		arch,
+		packageName,
+	})
 
 	apiResponse, err := client.Do()
 	if err != nil {
-		fmt.Errorf("%s", err.Error())
-		os.Exit(1)
+		reportError(err)
 	}
 
 	output := apiResponse.String()
@@ -93,11 +119,16 @@ func main() {
 	if jsonOutput {
 		output, err = apiResponse.toJSON()
 		if err != nil {
-			panic(err)
+			reportError(err)
 		}
 	}
 
 	if output != "" {
 		fmt.Println(output)
 	}
+}
+
+func reportError(err error) {
+	fmt.Printf("%s", err.Error())
+	os.Exit(1)
 }
--- repo_client.go
+++ repo_client.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
+	"mime/multipart"
 	"net/http"
+	"os"
 
 	"github.com/kovetskiy/toml"
 )
@@ -15,8 +18,17 @@ type APIResponse struct {
 	Data    map[string][]string `json:"data"`
 }
 
+const (
+	formPackageFile = "package_file"
+)
+
 func (response *APIResponse) String() string {
+	if !response.Success {
+		return response.Error
+	}
+
 	output := ""
+
 	for _, list := range response.Data {
 		if len(list) == 0 {
 			return ""
@@ -24,11 +36,13 @@ func (response *APIResponse) String() string {
 
 		for index, element := range list {
 			output = output + element
+
 			if index != len(list)-1 {
 				output = output + "\n"
 			}
 		}
 	}
+
 	return output
 }
 
@@ -42,10 +56,11 @@ func (response *APIResponse) toJSON() (string, error) {
 }
 
 type RepoClient struct {
-	repodURL string
-	method   string
-	resource string
-	body     io.Reader
+	repodURL    string
+	method      string
+	resource    string
+	body        bytes.Buffer
+	packageFile *os.File
 }
 
 func NewRepodClient(address string, version string) *RepoClient {
@@ -79,13 +94,29 @@ func (client *RepoClient) Do() (APIResponse, error) {
 	return apiResponse, nil
 }
 
+func (client *RepoClient) LoadPackageFile(packageFile string) error {
+	var err error
+
+	client.packageFile, err = os.Open(packageFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (client *RepoClient) doRequest() (*http.Response, error) {
 	var (
 		err error
 		url = client.repodURL + client.resource
 	)
 
-	request, err := http.NewRequest(client.method, url, client.body)
+	request, err := http.NewRequest(client.method, url, &client.body)
+	if err != nil {
+		return &http.Response{}, err
+	}
+
+	err = client.loadForm(request)
 	if err != nil {
 		return &http.Response{}, err
 	}
@@ -100,18 +131,40 @@ func (client *RepoClient) doRequest() (*http.Response, error) {
 	return response, nil
 }
 
-func (client *RepoClient) appendURLParts(
-	repo string,
-	epoch string,
-	db string,
-	arch string,
-) {
-	resources := []string{repo, epoch, db, arch}
-	for _, resource := range resources {
-		if resource == "" {
+func (client *RepoClient) loadForm(request *http.Request) error {
+	if client.packageFile == nil {
+		return nil
+	}
+
+	form := multipart.NewWriter(&client.body)
+	defer form.Close()
+
+	formWriter, err := form.CreateFormFile(
+		formPackageFile, client.packageFile.Name(),
+	)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(formWriter, client.packageFile); err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", form.FormDataContentType())
+
+	return nil
+}
+
+func (client *RepoClient) appendURLParts(parts []string) {
+	for index, part := range parts {
+		if part == "" {
 			break
 		}
 
-		client.resource = client.resource + resource + "/"
+		client.resource = client.resource + part
+
+		if index != len(parts)-1 {
+			client.resource = client.resource + "/"
+		}
 	}
 }
